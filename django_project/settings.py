a97a9412070b6b7e3615820e9fe4c047c0c5c1db
Abhijith <abhijithdev@tuta.io>
2021-02-03 13:34:07 +0530

    debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import dj_database_url
 import dotenv
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -137,5 +136,3 @@ LOGIN_URL='login'
 
 MEDIA_URL='/meida/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'meida')
-
-django_heroku.settings(config=locals(), staticfiles=False,logging=False)
